d4c7bdd842217c642ba61a01c15078b47972eded
vighneshnayakformal <vighneshnayakformal@gmail.com>
2025-10-21 22:54:52 +1300

    Remove unnecessary details from AboutSection

--- src/components/Sections/AboutSection.tsx
+++ src/components/Sections/AboutSection.tsx
@@ -1,7 +1,7 @@
 const AboutSection = () => {
   return (
     <div style={{ fontSize: "72px", textAlign: "center" }}>
-      This is QuizMaker, and this is my project for the Maths 302 Presentation!
+      This is QuizMaker.
     </div>
   );
 };
